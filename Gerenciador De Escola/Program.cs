00771ebfac55268a53f9310ce1f99cf72579d79c
AJGames4399 <AJ_Games@hotmail.com.br>
2023-10-13 23:09:00 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciador De Escola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gerenciador_De_Escola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
